5369cbe6f7bfb2b024c10715b37aafa79e50c2fd
ORaNGEMINT <72439347+OrangeMinnto@users.noreply.github.com>
2026-04-17 14:43:41 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,47 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	private int gear = 1;		//1速から5速のギアを表す
+	private int speed = 10;		//ギアチェンジ後の速度を表す
+	
+	
+	public void changeGear(int afterGear) {
+		int tempGear = this.gear;
+		this.gear = afterGear;
+		System.out.println("ギア" + tempGear +"から" + this.gear + "に切り替えました");
+		
+		switch(afterGear) {
+		case(1) -> {
+			this.speed = 10;
+			break;
+		}
+		case(2) -> {
+			this.speed = 20;
+			break;
+		}
+		case(3) -> {
+			this.speed = 30;
+			break;
+		}
+		case(4) -> {
+			this.speed = 40;
+			break;
+		}
+		case(5) -> {
+			this.speed = 50;
+			break;
+		}
+		default -> {
+			this.speed = 10;
+			break;
+		}
+		}
+	}
+	
+	
+	public void run() {
+		System.out.println("速度は時速" + speed +"kmです");
+	}
+
+
+}
