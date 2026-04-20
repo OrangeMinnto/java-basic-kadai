4116ae926889f77b1458d4ba88a2eeaba2ed3300
ORaNGEMINT <72439347+OrangeMinnto@users.noreply.github.com>
2026-04-20 09:27:05 +0900

    Add files via upload

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,23 @@
+package kadai_021;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dictionary_Chapter21 {
+	
+	HashMap<String, String> fruitMap = new HashMap<>(Map.of(
+				"apple", "りんご",
+				"peach", "桃",
+				"banana", "バナナ",
+				"lemon", "レモン",
+				"pear", "梨",
+				"kiwi", "キウィ",
+				"strawberry", "いちご",
+				"grape", "ぶどう",
+				"muscat", "マスカット",
+				"cherry", "さくらんぽ"
+			));
+	
+	
+
+}
